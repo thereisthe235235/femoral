e4f46f1d63eff19f40744212de16a23e962236ca
David Jensen <DavidJensendl@gmail.com>
2023-09-11 20:58:35 -0600

    edit11

---
+++ DAGV 1400 X01/Unit 1b — Lab.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {      int x = 20 - 1;
+      string str1 = "there";
+      string str2 = " was an attept";
+      int r = x;
+      double fl1 = 3.333;
+      double fl2 = 6.667;
+      bool bo1 = true;
+      bool bo2 = false;
+      Console.WriteLine(r);
+      Console.WriteLine(str1 + str2);
+      Console.WriteLine(fl1 / fl2);
+      Console.WriteLine(bo1);
+       // here im difining my varibles and then doing diffrent opration from them, besides bool, it doesnt seem to work//
+    Console.WriteLine("whats your name?");
+      string Teeth = Console.ReadLine();
+      Console.WriteLine("hello" + Teeth);
