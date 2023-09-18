1ce5a2153e1d4899b948d4e7e698f2c4a8d7b6d8
David Jensen <DavidJensendl@gmail.com>
2023-09-18 09:48:17 -0600

    editww

---
+++ DAGV 1400 X01/Unit 1c — Lab .cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	int[] myNum = {1, 100};
+		Console.WriteLine("how many rows?");
+int row = Convert.ToInt32(Console.ReadLine());
+		for (int i = 1; i <= row; i++) 
+{
+			Console.WriteLine(i);
+}
+	}
+}
+// i coudnt figure out how to do it. can you send a working example so i can reverse enginer it so i know how to do in the future thx you
